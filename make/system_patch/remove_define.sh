65b02d5e5c62328a5cac1fdee977a3046be07724
Abhi <78593493+yash1375@users.noreply.github.com>
2023-03-10 05:51:49 +0530

    Delete remove_define.sh

--- make/system_patch/remove_define.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-LOCALDIR=`cd "$( dirname ${BASH_SOURCE[0]} )" && pwd`
-cd $LOCALDIR
-
-target_fs="$LOCALDIR/add_repack_fs"
-target_contexts="$LOCALDIR/add_repack_contexts"
-
-sed -i '/bin\/netd/d' $target_fs
-sed -i '/bin\/netd/d' $target_contexts
